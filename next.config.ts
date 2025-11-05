32f0818280bd10ad1d561c8fdf1f48a468de9cad
Zyko <modaherry@gmail.com>
2025-11-05 15:01:49 +0100

    correction webpack et ajout d'un type de commande

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  productionBrowserSourceMaps: false, // Désactive les source maps en production
-  webpack: (config: import('webpack').Configuration, { dev, isServer }: { dev: boolean; isServer: boolean }) => {
-    // Désactive les source maps en développement
-    if (!dev) {
-      config.devtool = false;
-    }
-    return config;
-  },
-  images: {
-    unoptimized: true
-  }
-}
+import type { NextConfig } from 'next'
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  experimental: {
+    turbo : {
+    }
+  },
+}
+ 
+export default nextConfig
